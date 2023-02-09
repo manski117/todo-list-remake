dd229c053dd4fd97c4eae1cb48755f70cf4a2faf
manski117 <choromanski117@gmail.com>
2023-02-08 22:56:59 -0500

    Add delete button to edit project form

--- src/components/EditProjectForm.tsx
+++ src/components/EditProjectForm.tsx
@@ -49,6 +49,16 @@ export default function EditProjectForm(props: any){
       setAllProjects(newAllProjects); 
     }
 
+    function deleteProject(){
+      //create a copy of the state
+      let newAllProjects = {...allProjectsCopy};
+      //delete old project in the object
+      delete newAllProjects[`${props.oldProjectName}`];
+      //set state equal to new object
+      setAllProjects(newAllProjects);
+      props.handleclick(); 
+    }
+
     class Project {
         title: string;
         tasks: {};
@@ -68,7 +78,7 @@ export default function EditProjectForm(props: any){
           <button onClick={props.handleClick} className='close-btn' aria-label="Close Form">X</button>
           <Formik
             initialValues={{
-              projectName: ''
+              projectName: props.oldProjectName
             }}
             validationSchema={editProjectSchema}
             onSubmit={(values: Values, { setSubmitting, resetForm }: FormikHelpers<Values>) => {
@@ -88,9 +98,12 @@ export default function EditProjectForm(props: any){
           >
             <Form className="Form formik-form">
               <label htmlFor="projectName">Edit Project Name</label>
-              <Field className='Field text-input' id="projectName" name="projectName" placeholder="Give your project a new name" />
-              <button type="submit">&#9989;</button>
-              <ErrorMessage name="projectName">{msg => <div className="error-feedback error-msg">{msg}</div>}</ErrorMessage>
+              <span className="edit-input-container flexbox">
+                <Field className='Field text-input' id="projectName" name="projectName" placeholder="Give your project a new name" />
+                <button title="confirm changes" type="submit">&#9989;</button>
+                <button title="delete project" onClick={deleteProject} className="delete-btn">&#128465;</button>
+              </span>
+              <ErrorMessage name="projectName">{msg => <div className="error-feedback error-msg-edit-project">{msg}</div>}</ErrorMessage>
               
               
             </Form>
